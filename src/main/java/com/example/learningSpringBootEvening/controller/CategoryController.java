779c16fa8187ff7f0105591c3e9a184ad629f2d9
lanos-ics <lanos.institute@gmail.com>
2026-01-29 19:04:20 +0530

    introduced response as JSON using Request Body and added a new get method to handle single category using id

--- src/main/java/com/example/learningSpringBootEvening/controller/CategoryController.java
+++ src/main/java/com/example/learningSpringBootEvening/controller/CategoryController.java
@@ -1,38 +1,51 @@
 package com.example.learningSpringBootEvening.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.learningSpringBootEvening.model.Category;
 import com.example.learningSpringBootEvening.service.CategoryService;
 
 @RestController
 public class CategoryController {
 	
-	ArrayList<String> categories = new ArrayList<>();
+	ArrayList<Category> categories = new ArrayList<>(); // DB
 	
-//	@Autowired
-//	private CategoryService categoryService;
 	
 	// to Get all the categories in the DB : READ/SHOW/DISPLAY
 	
+	@GetMapping("api/v1/categories/{catId}")
+	public List<Category> getCategory(@PathVariable Long catId)
+	{
+		List<Category> category = categories.stream().filter(cat -> cat.getId() == catId).toList() ;
+		
+		
+		return category;
+	}
+	
 	@GetMapping("/api/v1/categories")
-	public ArrayList<String> getCategories()
+	public ArrayList<Category> getCategories()
 	{
 		return categories;
 	}
 	
 	// to add a new Category to the DB :  CREATE
 	
-	@PostMapping("api/v1/categories/{catName}")
-	public String addCategory(@PathVariable String categoryName)
+	@PostMapping("api/v1/categories")
+	public String addCategory(@RequestBody Category newCategory)
 	{
-		categories.add(categoryName);
-		return "Category " + categoryName + " added sucessfully!";
+		categories.add(newCategory);
+		return "Category "+ newCategory.getCategoryName() + " added sucessfully!";
 	}
 	
 	
